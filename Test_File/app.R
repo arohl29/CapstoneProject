b33a1e18ed00dc4bae9198547459736317cc2a0e
Germán <gperez1997@email.arizona.edu>
2022-02-27 18:40:08 -0700

    Work on creating interactive options on sidebarPanel design; and looked into customizing text, color, etc. Wasn't able to work with API because of confusion. Contacted Adriana about it, see if we can figure that out.

--- Test_File/app.R
+++ Test_File/app.R
@@ -8,41 +8,60 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(bslib)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(title = "Recommendations",
+  theme = bslib::bs_theme(bootswatch = "solar"),
 
-    # Application title
-    titlePanel("Capstone Project"),
+  # Application title
+  titlePanel("Watch Recommendations"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      wellPanel(
+        textOutput("welcome"), class = "btn-lg"
+      ),
+      inputPanel(
+        checkboxGroupInput("movieSeries", "Movies and/or Series?",
+                          choices = c("Movies", "Series")),
+        textInput("actor", "Actor in Movie/Series (Optional)",
+                  placeholder = "Enter actor"),
+        numericInput("time", "Time available (in hours)", 0,
+                    min = 0, max = 10), # maybe drop-down list, or slider?
+        checkboxGroupInput("ratings", "Movie Rating",
+                            choices = c("G", "PG", "PG-13", "R"))
+      )
+    ),
+      
+    mainPanel()
+  )
+    
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+        # mainPanel(
+        #    plotOutput("distPlot")
+        # )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  output$welcome <- renderText("Welcome to our Movie/Series Recommendation System!
+                     Please fill out the information below so that we can
+                     give you recommendations based on your choices. Some
+                     of the choices are optional, such as the Actor Name,
+                     and your time available which will give you movies
+                     or series of any duration length.")
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # })
 }
 
 # Run the application 
