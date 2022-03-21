97d88f91b00ac649ed1be4748788e9ec50692b37
Germán <gperez1997@email.arizona.edu>
2022-03-20 20:21:36 -0700

    Tried cleaning data tables for movies and series but 'col_select' wouldn't work. I added a function (data_search()) that will process the tables based on the choice but wasn't able to pass the ratings and moive/series choices vector as parameters (might need to make that element global). Syntax of the function should work to process the tables, its just the matter of figuring out the whole parameter thing. And added another file with similar code to get the series data; you can change the value for the range (I did 10000, again).

--- Test_File/app.R
+++ Test_File/app.R
@@ -11,10 +11,16 @@ library(shiny)
 library(tidyverse)
 library(bslib)
 
-data <- read.csv("data/themoviedb-api-data.csv")
-glimpse(data)
-timerange <- data$runtime[order(data$runtime)]
+# Select only columns from data file that we need
+movie_data <- read.csv("data/themoviedb-api-data.csv")
+tv_data <- read.csv("data/themoviedb-tv-data.csv")
+
+glimpse(movie_data)
+glimpse(tv_data)
+timerange <- movie_data$runtime[order(movie_data$runtime)]
 range1 <- timerange[0:10]
+timerange <- tv_data$runtime[order(tv_data$runtime)]
+range2 <- timerange[0:10]
 
 # Define UI for application
 ui <- fluidPage(title = "Recommendations",
@@ -27,32 +33,33 @@ ui <- fluidPage(title = "Recommendations",
   sidebarLayout(
     sidebarPanel(
       wellPanel(
-        textOutput("welcome"), class = "btn-lg" # Welcome message (might be info?)
+        textOutput("welcome"), class = "btn-lg" # Welcome message (instructions?)
       ),
       inputPanel(
-        #textOutput("movieSeries"),
-        #checkboxInput("movies", "Movies?"),
-        #checkboxInput("series", "Series?"),
         
-        #Options to look for movies, series, or both
-        radioButtons("movieSeries", "Movies and/or Series?",
-                     c("Movies" = "movies", "Series" = "series",
-                       "Both" = "both")),
+        # Options to look for movies, series, or both
+        checkboxGroupInput("movieSeries", "Movies and/or Series?",
+                           choices = c("Movies", "Series")),
         
-        #Option to look for an actor/actress
+        # Option to look for an actor/actress
+        # Might need to remove this option (current API doesn't have this info)
         textInput("actor", "Actor in Movie/Series (Optional)",
                   placeholder = "Enter actor"),
         
+        # Can be multiplied when processing
+        # Time in data is in minutes (double)
         sliderInput("time", "Time available (in hours)",
-                    min = 0, max = 120, value = 0), # maybe drop-down list, or
+                    min = 0, max = 120, value = c(0, 120)), # maybe drop-down list, or
                                                     # slider?
         
-        checkboxGroupInput("ratings", "Movie Rating",
+        checkboxGroupInput("ratings", "Age Rating",
                             choices = c("G", "PG", "PG-13", "R")),
-        checkboxGroupInput("popular", "Popular Rating",
-                           choices = c("1", "2", "3", "4"))
+        
+        # Rating can be done as a slider that allows for range
+        sliderInput("popular", "Movie/Series Rating",
+                    min = 0.0, max = 10.0, value = c(0.0, 10.0))
       ),
-      submitButton("Search!") # Need to center button
+      submitButton("Search!") # Need to center button (?)
       # actionButton() can be used to reset values?
     ),
       
@@ -66,6 +73,30 @@ ui <- fluidPage(title = "Recommendations",
   )
 )
 
+# This function will take whichever radio option is used and
+# based on that go through either or both data files to get
+# the titles of the movies and/or series
+data_search <- function(movie_or_series) {
+  file = NA
+  if (length(movie_or_series) == 1) {
+    if ("movies" %in% movie_or_series){
+      renderText("Looking for movies.")
+      file = movie_data
+    }
+    if ("series" %in% movie_or_series) {
+      renderText("Looking for series.")
+      file = tv_data
+    } 
+  } else {
+    # Process both files for series and movies
+  }
+  
+  if ((file$vote_average %in% ratings) && (file$runtime %in% time*60)) {
+    renderText(paste(file$title), sep = "\n")
+  }
+  
+}
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
@@ -78,23 +109,24 @@ server <- function(input, output) {
   
   #activateApp <- eventReactive()
   
-  output$test <- renderText(switch (input$movieSeries,
-                                  movies = "Looking for movies",
-                                  series = "Looking for series",
-                                  both = "Looking for both"))
+  # Depending on choice, process data frames (movies or series, or both)
+  # Call data processing function
+  output$test <- data_search("movies")
+  # Might need to make ui elements as global to be able to use them
+  # (if it is possible)
   
   output$time <- renderText(if (input$time == 0){
-    paste(range1)
+    paste(range1, "\n", range2)
   })
   
-  output$rating <- renderText(paste("Ratings: ",input$ratings)) # Vector of choices
+  output$rating <- renderText(input$ratings) # Vector of choices
                               #columnName %in% input$ratings -> do something
   
-  output$person <- renderText(paste("Looking for: ",input$actor))
+  #output$person <- renderText(paste("Looking for: ",input$actor))
   
-  output$popular <- renderText(if (input$popular == '1'){
-    paste(data$popularity)
-  })
+  #output$popular <- renderText(if (input$popular == '1'){
+  #  paste(data$popularity)
+  #})
 
 }
 
